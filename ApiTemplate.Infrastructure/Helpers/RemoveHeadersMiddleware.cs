84e27a95fc67526b00a0ce4a5be22644f86ad9f1
Solomon Uke <engineer@rikudouensof.com>
2024-09-24 18:44:12 +0100

    Add project files.

---
+++ ApiTemplate.Infrastructure/Helpers/RemoveHeadersMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTemplate.Infrastructure.Helpers
+{
+    public class RemoveServerHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RemoveServerHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.OnStarting(() =>
+            {
+                context.Response.Headers.Remove("Server");
+                return Task.CompletedTask;
+            });
+
+            await _next(context);
+        }
+    }
+
+}
---
+++ ApiTemplate.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ApiTemplate.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
