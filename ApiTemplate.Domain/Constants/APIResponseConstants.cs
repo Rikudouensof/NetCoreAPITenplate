9ba9d58b2b2aad437ecf963e83ddc17087993527
Solomon Uke <engineer@rikudouensof.com>
2024-09-24 19:29:07 +0100

    Add Core Response Codes

---
+++ ApiTemplate.Domain/Constants/APIResponseConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTemplate.Domain.Constants
+{
+    public static class APIResponseConstants
+    {
+        public const string sucessCode = "00";
+        public const string sucessMessage = "Sucessfull";
+        public const string failureCode = "01";
+        public const string failureMessage = "Failed";
+        public const string timeoutCode = "02";
+        public const string timeoutMessage = "Service Timeout";
+    }
+}
