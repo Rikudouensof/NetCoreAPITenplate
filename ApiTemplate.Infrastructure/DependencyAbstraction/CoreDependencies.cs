6d67217b20e6a4540f0289dfdd8ae9e56038e87c
Solomon Uke <engineer@rikudouensof.com>
2024-09-24 18:58:08 +0100

    Add the Core dependecy abstraction

---
+++ ApiTemplate.Infrastructure/DependencyAbstraction/CoreDependencies.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTemplate.Infrastructure.DependencyAbstraction
+{
+    public static class CoreDependencies
+    {
+
+
+        public static IServiceCollection ImplementCoreDependencies(this IServiceCollection service)
+        {
+            
+
+            return service;
+        }
+    }
+}
