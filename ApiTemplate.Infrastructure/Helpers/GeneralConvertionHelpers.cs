d1203995cb2b6b2aa04be5b94aa6649677417e47
Solomon Uke <engineer@rikudouensof.com>
2024-09-24 19:33:56 +0100

    Add common convertions and generators

---
+++ ApiTemplate.Infrastructure/Helpers/GeneralConvertionHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ApiTemplate.Infrastructure.Helpers
+{
+    public static class GeneralConvertionHelpers
+    {
+        public static string DateTimeToString(DateTime input)
+        {
+            return input.ToString("HH:mm ddd dd/MM/yyyy");
+        }
+
+        public static byte[] stringToyByteArray(string inputString)
+        {
+            return Encoding.ASCII.GetBytes(inputString);
+        }
+
+        public static string byteArrayToString(byte[] inputByte)
+        {
+            return Encoding.ASCII.GetString(inputByte);
+        }
+
+    }
+}
---
+++ ApiTemplate.Infrastructure/Helpers/GeneralGeneratorHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTemplate.Infrastructure.Helpers
+{
+    public static class GeneralGeneratorHelpers
+    {
+        public static string GetNewRequestId()
+        {
+            Random rnd = new Random();
+            int myRandomNo = rnd.Next(1000000, 9999999);
+            var requestId = $"Track:{myRandomNo}";
+            return requestId;
+        }
+    }
+}
